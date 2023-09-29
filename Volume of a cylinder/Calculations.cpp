0c3e7309244098e51c9f07992ebe1a99d943ffca
aausborn1 <aausborn21@gmail.com>
2023-09-29 09:40:19 -0500

    Add project files.

---
+++ Volume of a cylinder/Calculations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main() {
+	double radius, height, volume;
+	cout << "Radius:";
+	cin >> radius;
+		cout << "Height :";
+		cin >> height;
+
+		double; volume = 3.14 * 2(radius, height);
+	cout << "Volume:" << volume << endl;
+	return 0;
+
+}
